fbfd717499aeac24ec4ca986d7e91cfa7f498216
Sherexmykes <sherexmykes@gmail.com>
2020-07-08 23:28:19 -0400

    finished

---
+++ src/iterative_sorting/challenge.py
@@ -0,0 +1,22 @@
+
+
+arr = [[8, 4], [90, -1, 3], [9, 62], [-7, -1, -56, -6], [201], [76, 18]];
+
+
+def bubble_sort(arr):
+    # Your code here
+    for i in range(len - 1):
+        for j in range(0, len - 1):
+            if arr[j] > arr[j + 1]:
+                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+    print(arr)     
+                   
+##loop outer array
+##return
+##loop inner array
+##pull out smallest numbers in each inner array
+##add sum of each smaller number
+##print or return the sum of 175
+
+
